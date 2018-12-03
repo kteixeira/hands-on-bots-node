3b4c993df2f880c5a6e8d152eef1261d27116460
Kaio Leal <kaio.teco@gmail.com>
2018-12-03 17:10:25 -0200

    finalizando modulo 2

--- src/Modulo-2/conversacao.js
+++ src/Modulo-2/conversacao.js
@@ -6,19 +6,33 @@
  */
 
 const builder = require('botbuilder');
+const restify = require('restify');
 
-const connector = new builder.ConsoleConnector().listen();
-const bot = new builder.UniversalBot(connector);
+//Configurações do server via restify
+const server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, () => {
+    console.log('%s Aplicação executando na porta %s', server.name, server.url);
+});
 
-bot.dialog('/', [
+const connector = new builder.ChatConnector({
+    appId: '',
+    appPassword: ''
+})
+
+const bot = new builder.UniversalBot(connector, [
     (session) => {
-        builder.Prompts.text(session, "Oi! Tudo bem?");
+        builder.Prompts.text(session, 'Olá! Tudo bem?')
     },
+
     (session) => {
-        builder.Prompts.text(session, "Qual é o seu nome?")
+        builder.Prompts.text(session, 'Como você se chama?')
     },
+
     (session, results) => {
         let msg = results.response;
         session.send(`Oi ${msg}! Em que posso ajudar?`)
-    } 
-])
+    }
+]);
+
+//Endpoint para executar as mensagens para o usuário
+server.post('/api/messages', connector.listen());
